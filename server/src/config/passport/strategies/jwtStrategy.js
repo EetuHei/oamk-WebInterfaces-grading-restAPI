b1afef98120d38c92bdd67983e69965d888f377b
EetuHei <t8heee01@students.oamk.fi>
2020-02-19 13:04:22 +0200

    jwt strategy

---
+++ server/src/config/passport/strategies/jwtStrategy.js
@@ -0,0 +1,24 @@
+const JwtStrategy = require('passport-jwt').Strategy;
+const ExtractJwt = require('passport-jwt').ExtractJwt;
+const httpErrors = require('http-errors');
+const authServices = require('../../../auth/authServices');
+
+const jwtStrategy = new JwtStrategy(
+  {
+    jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+    secretOrKey: 'potato', //process.env.PASSPORT_SECRET
+    passReqToCallback: true
+  },
+  async (req, payload, done) => {
+    const { knex } = req.context;
+
+    try {
+      const user = await authServices.authenticateFromPayload(knex, payload);
+      return done(null, user);
+    } catch (e) {
+      return done(httpErrors(401));
+    }
+  }
+);
+
+module.exports = jwtStrategy;
