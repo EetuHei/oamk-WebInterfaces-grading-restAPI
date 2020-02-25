0ab8356290ed2f29e2f82d4ab088be0260bc1622
EetuHei <t8heee01@students.oamk.fi>
2020-02-25 13:09:37 +0200

    added few more default users for the seed

--- server/seeds/01_user.js
+++ server/seeds/01_user.js
@@ -4,33 +4,56 @@ const seed = async knex => {
   const mainUsers = [
     {
       username: 'root',
-      name: 'root',
-      address: 'kotkantie 1',
-      city: 'oulu',
-      country: 'Fi',
       email: 'root@lmao.lol',
-      phoneNumber: '1231231233',
-      password: '111111'
+      password: '111111',
+      name: 'root boi',
+      address: 'roothome',
+      city: 'api',
+      country: 'interwebz',
+      phoneNumber: '1231231233'
+    },
+    {
+      username: 'eetu',
+      email: 'eetu@lmao.lol',
+      password: '111111',
+      name: 'Eetu epe',
+      address: 'kotkantie 3',
+      city: 'Oulu',
+      country: 'Fi',
+      phoneNumber: '1231231232'
+    },
+    {
+      username: 'riku',
+      email: 'riku@lmao.lol',
+      password: '111111',
+      name: 'riku rauski',
+      address: 'kotkantie 1',
+      city: 'Oulu',
+      country: 'Fi',
+      phoneNumber: '1231231231'
     }
   ];
 
-  const registerTasks = [...Array(200).keys()].map(number => {
-    return knex =>
-      authServices.registerUser(knex, {
-        username: `user${number}`,
-        name: `user${number}`,
-        address: `user${number}`,
-        city: `user${number}`,
-        country: `user${number}`,
-        email: `user${number}@lmao.lol`,
-        phoneNumber: `user${number}`,
-        password: '111111'
-      });
-  });
+  // script to add random users
+
+  // const registerTasks = [...Array(200).keys()].map(number => {
+  //   return knex =>
+  //     authServices.registerUser(knex, {
+  //       username: `user${number}`,
+  //       email: `user${number}@lmao.lol`,
+  //       password: '111111',
+  //       name: `user${number}`,
+  //       address: `user${number}`,
+  //       city: `user${number}`,
+  //       country: `user${number}`,
+  //       phoneNumber: `user${number}`
+  //     });
+  // });
 
   await knex.transaction(async trx => {
     await Promise.all([
-      ...mainUsers.map(user => authServices.registerUser(trx, user)) //,...registerTasks.map(task => task(trx))  script for random users
+      ...mainUsers.map(user => authServices.registerUser(trx, user))
+      // ...registerTasks.map(task => task(trx))
     ]);
   });
 };
