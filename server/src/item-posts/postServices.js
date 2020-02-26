763ec6bc5956bc8d5ec25b9fa668ee40cc49fca2
Riku Heikkinen <riku.heikkinen94@gmail.com>
2020-02-26 13:09:22 +0200

    image adding

--- server/src/item-posts/postServices.js
+++ server/src/item-posts/postServices.js
@@ -1,5 +1,8 @@
 const Post = require('./Post');
 const postQueries = require('./postQueries');
+const multer = require('multer');
+const upload = multer({ dest: '../uploads' });
+const fs = require('fs');
 
 const getPosts = async knex => {
   const getPostsData = await postQueries.getPosts();
@@ -10,7 +13,40 @@ const getPosts = async knex => {
 };
 
 const addImage = () => {
-  return 'asd';
+  postRouter.put(
+    '/add-image/:id',
+    addImage,
+    upload.array('img', 4),
+    (req, res) => {
+      const images = req.files;
+
+      images.forEach(img => {
+        fs.rename(img.path, `./uploads/${img.originalname}`, err => {
+          if (err) throw err;
+          console.log('renamed images');
+        });
+      });
+
+      images.img = images;
+
+      images
+        .save()
+        .then(images =>
+          res.status(200).json({
+            images,
+            status: 'Images added',
+            success: true
+          })
+        )
+        .catch(err => {
+          console.log(err);
+          res.status(401).json({
+            status: 'Unauthorized',
+            success: false
+          });
+        });
+    }
+  );
 };
 
 const postItem = async (
