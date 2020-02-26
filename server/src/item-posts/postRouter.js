c8b36d421a82aad6ad45383c5f7eeb49cd47371a
Riku Heikkinen <riku.heikkinen94@gmail.com>
2020-02-26 16:59:39 +0200

    added route to upload images with multer, added route to edit posts

--- server/src/item-posts/postRouter.js
+++ server/src/item-posts/postRouter.js
@@ -3,6 +3,11 @@ const passport = require('../config/passport/passport');
 const postValidations = require('./postValidations');
 const postServices = require('./postServices');
 const postQueries = require('./postQueries');
+const multer = require('multer');
+const upload = multer({ dest: './uploads/' });
+const fs = require('fs');
+// const host = req.host;
+// const filePath = req.protocol + '://' + host + '/' + req.file.path;
 const postRouter = new Router();
 
 const getPosts = async (req, res, next) => {
@@ -18,13 +23,12 @@ const getPosts = async (req, res, next) => {
 };
 
 const postItem = async (req, res, next) => {
-  const { knex, postData, addImage } = req.context;
+  const { knex, postData } = req.context;
 
   try {
     const post = await postServices.postItem(knex, postData);
-    const image = await postServices.addImage(knex, addImage);
 
-    return res.data(201, { post, image });
+    return res.data(201, { post });
   } catch (e) {
     if (e.code === 'ER_DUP_ENTRY') {
       const forbiddenError = httpErrors(403, 'Post already exists');
@@ -34,12 +38,55 @@ const postItem = async (req, res, next) => {
   }
 };
 
+postRouter.put(
+  '/add-image/:id',
+  upload.array('img', 4),
+  passport.authenticate('jwt', { session: false }),
+  postValidations.imageUploadData,
+  async (req, res, next) => {
+    const images = req.files;
+    const { knex, addImagesData } = req.context;
+
+    images.forEach(img => {
+      fs.rename(img.path, `./uploads/${img.originalname}`, err => {
+        if (err) throw err;
+        console.log('image rename success');
+        try {
+          const data = postServices.editPostImage(knex, addImagesData);
+          return res.data(null, { data });
+        } catch (e) {
+          throw e;
+        }
+      });
+    });
+  }
+);
+
 postRouter.post(
   '/create-post',
   passport.authenticate('jwt', { session: false }),
   postValidations.createPost,
   postItem
 );
+
+postRouter.put(
+  '/edit-post/:id',
+  passport.authenticate('jwt', { session: false }),
+  [
+    postValidations.postUpdate,
+    async (req, res, next) => {
+      const { knex, editPostData } = req.context;
+
+      try {
+        const data = await postServices.editPost(knex, editPostData);
+        return res.data(null, { data });
+      } catch (e) {
+        throw e;
+      }
+    }
+  ]
+);
+
 postRouter.get('/all-posts', getPosts);
 
 module.exports = postRouter;
